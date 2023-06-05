f3a051b2ac1d8bf4abad1c0e01831e38a5c8971a
Natália Fernandes <88285066+fernandesnatalia@users.noreply.github.com>
2023-06-05 13:48:13 -0300

    Delete steps

--- snake.py
+++ snake.py
@@ -1,12 +1,9 @@
-# importar e dar inicio
-
 import pygame, random
 from pygame.locals import *
 pygame.init()
 screen = pygame.display.set_mode((600,600))
 pygame.display.set_caption('snake')
 
-# função para movimentar dentro do grid
 def on_grid_random():
     x = random.randint(0, 590)
     y = random.randint(0, 590)
@@ -15,34 +12,28 @@ def on_grid_random():
 def collision(c1, c2):
     return (c1[0] == c2[0] and (c1[1] == c2[1]))
 
-# definir as direções
 UP = 0
 RIGHT = 1
 DOWN = 2
 LEFT = 3
 
-# definir tamanho da tela e cobra
 snake = [(200,200), (210, 200), (220, 200)]
 skin = pygame.Surface((10, 10))
 skin.fill((255, 255, 255))
 
-#definir tamanho, cor e direção da maçã
 apple = pygame.Surface((10, 10))
 apple.fill((255, 0, 0))
 applePosition = on_grid_random()
 caminho = LEFT
 
-# função para desacelerar a movimentação
 clock = pygame.time.Clock()
 
-# traço de repetição do jogo
 while True:
     clock.tick(10)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
 
-# função para controlar a cobra
     if event.type == KEYDOWN:
         if event.key == K_UP:
             caminho = UP
@@ -53,16 +44,13 @@ while True:
         if event.key == K_LEFT:
             caminho = LEFT
 
-# definir que a cobra cresça quando colidir com a maça
     if collision(snake[0], applePosition):
         applePosition = on_grid_random()
         snake.append((0, 0))
 
-# movimentação do final da cobra
     for i in range(len(snake) - 1, 0, -1):
         snake[i] = (snake[i - 1][0], snake[i - 1][1])
 
-# movimentação da cabeça da cobra [0]
     if caminho == UP:
         snake[0] = (snake[0][0], snake[0][1] - 10)
     if caminho == RIGHT:
